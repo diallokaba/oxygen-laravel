aad12c28c02c07d3e36daf690e2014bc3b33bbb7
diallo ibrahima kaba <dialloibhazard@gmail.com>
2024-11-10 20:49:24 +0000

    resolving error

--- app/Services/GenerateQrcodeService.php
+++ app/Services/GenerateQrcodeService.php
@@ -25,12 +25,13 @@ class GenerateQrcodeService{
         
         // Chemin pour enregistrer le QR code temporaire
         $tempDir = storage_path('app/temp'); // Utilisation de storage_path() pour obtenir le chemin absolu
-        $qrTempPath = $tempDir . '/qrcode_' . uniqid() . '.png';
 
         // Vérifiez si le répertoire existe, sinon créez-le
         if (!file_exists($tempDir)) {
             mkdir($tempDir, 0777, true); // Créez le répertoire s'il n'existe pas
         }
+        
+        $qrTempPath = $tempDir . '/qrcode_' . uniqid() . '.png';
 
         // Enregistrer le QR code temporaire
         file_put_contents($qrTempPath, $qrcode->getString()); // Utilisation de file_put_contents() pour écrire le fichier
